4d8a018604003716491b373928fce76de555ffe8
anca.morosan <anca.morosan@tu-berlin.de>
2026-06-17 18:55:39 +0200

    navigation button changed

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,24 +9,24 @@ const authStore = useAuthStore()
   <nav class="w-full flex items-center gap-6 px-10 py-4 border-b border-zinc-800 bg-zinc-950">
     <span class="text-white font-bold text-lg mr-4">TU Stock Exchange</span>
 
-    <RouterLink to="/market" class="text-gray-400 hover:text-white transition">Market</RouterLink>
-    <RouterLink to="/leaderboard" class="text-gray-400 hover:text-white transition">Leaderboard</RouterLink>
+    <RouterLink to="/market" class="nav-link">Market</RouterLink>
+    <RouterLink to="/leaderboard" class="nav-link">Leaderboard</RouterLink>
 
     <template v-if="authStore.isLoggedIn">
-      <RouterLink to="/dashboard" class="text-gray-400 hover:text-white transition">Dashboard</RouterLink>
-      <RouterLink to="/portfolio" class="text-gray-400 hover:text-white transition">Portfolio</RouterLink>
-      <RouterLink to="/auto-trades" class="text-gray-400 hover:text-white transition">Auto-trades</RouterLink>
-      <RouterLink to="/profile" class="text-gray-400 hover:text-white transition">Profile</RouterLink>
+      <RouterLink to="/dashboard" class="nav-link">Dashboard</RouterLink>
+      <RouterLink to="/portfolio" class="nav-link">Portfolio</RouterLink>
+      <RouterLink to="/auto-trades" class="nav-link">Auto-trades</RouterLink>
+      <RouterLink to="/profile" class="nav-link">Profile</RouterLink>
     </template>
 
     <div class="ml-auto flex gap-4">
       <template v-if="authStore.isLoggedIn">
         <span class="text-zinc-500 text-sm self-center">{{ authStore.user?.username }}</span>
-        <button @click="authStore.logout()" class="text-gray-400 hover:text-white transition">Logout</button>
+        <button @click="authStore.logout()" class="nav-link">Logout</button>
       </template>
       <template v-else>
-        <RouterLink to="/login" class="text-gray-400 hover:text-white transition">Login</RouterLink>
-        <RouterLink to="/register" class="text-gray-400 hover:text-white transition">Register</RouterLink>
+        <RouterLink to="/login" class="nav-link">Login</RouterLink>
+        <RouterLink to="/register" class="nav-link">Register</RouterLink>
       </template>
     </div>
   </nav>
@@ -35,3 +35,28 @@ const authStore = useAuthStore()
     <RouterView />
   </main>
 </template>
+
+<style scoped>
+/* css logic for task manager (all the pages) */
+/* normal */
+.nav-link {
+  color: #71717A;
+  text-decoration: none;
+  padding-bottom: 4px;
+  border-bottom: 2px solid transparent;  /* placeholder — unsichtbar */
+}
+
+/* hover */
+.nav-link:hover {
+  color: #EAB308;
+  border-bottom: 2px solid #EAB308;
+}
+
+/* actual page */
+.nav-link.router-link-active {
+  color: #EAB308;
+  border-bottom: 2px solid #EAB308;
+
+}
+
+</style>
